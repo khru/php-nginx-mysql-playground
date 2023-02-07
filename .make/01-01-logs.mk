9d9566802807096677398a3984d84da50fdeb9a6
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-02-07 19:09:27 +0100

    feat: adding connection to the shell of each container as well as update .env

--- .make/01-01-logs.mk
+++ .make/01-01-logs.mk
@@ -4,12 +4,14 @@
 logs: ## view all logs.
 	@docker-compose logs -f -t
 
-.PHONY: logs
+.PHONY: logs-php
 logs-php: ## view php logs.
 	@docker-compose logs -f -t $(APP_CONTAINER_NAME)
 
+.PHONY: logs-nginx
 logs-nginx: ## view nginx logs.
 	@docker-compose logs -f -t $(NGINX_CONTAINER_NAME)
 
+.PHONY: logs-db
 logs-db: ## view database logs.
 	@docker-compose logs -f -t $(DB_CONTAINER_NAME)
---
+++ .make/01-04-shells.mk
@@ -0,0 +1,17 @@
+##@ [Shells: Connect to each container]
+
+.PHONY: bash-php
+bash-php: ## Connect to PHP container.
+	docker-compose run --rm --service-ports -u $(DOCKER_APP_UUID) $(APP_CONTAINER_NAME) /bin/bash
+
+.PHONY: bash-db
+bash-db: ## Connect to BD container.
+	docker-compose run --rm --service-ports -u $(DOCKER_DB_UUID) $(DB_CONTAINER_NAME) /bin/bash
+
+.PHONY: bash-nginx
+bash-nginx: ## Connect to NGINX container.
+	docker-compose run --rm --service-ports -u $(DOCKER_NGINX_UUID) $(NGINX_CONTAINER_NAME) /bin/bash
+
+.PHONY: bash-composer
+bash-composer: ## Connect to COMPOSER container.
+	@docker-compose run --rm --service-ports -u $(DOCKER_COMPOSER_UUID) $(COMPOSER_CONTAINER_NAME) /bin/bash
